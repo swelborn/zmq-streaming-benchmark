c970e3cef340ebda998adb7814bbec115c77f267
Sam Welborn <swelborn@lbl.gov>
2023-07-03 17:44:53 -0700

    update to have multiple contexts

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,7 +3,7 @@
 cd /src/build
 
 n_msgs=1000000
-msg_size=200000
+msg_size=2000000
 n_ports=1
 port_base=5535
 num_io_threads=20
@@ -16,5 +16,5 @@ done
 # Loop through the array and run the command with each port
 # for port in "${PORTS[@]}"
 # do
-./push "tcp://$ZMQ_BIND_IP:$port_base" $msg_size $n_msgs $num_io_threads &
+./push "tcp://$ZMQ_BIND_IP:$port_base" $msg_size $n_msgs $num_io_threads
 # done
