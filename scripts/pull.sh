48d281d7b1f19ae9474189f3a355212ef2e73496
Sam Welborn <swelborn@lbl.gov>
2023-11-27 09:58:03 -0800

    update pull scripts

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -4,11 +4,11 @@ cd /src/build
 
 n_msgs=$1
 n_msgs=1000000
-msg_size=200000
+msg_size=20000
 n_ports=1
 port_base=5535
-num_io_threads=20
-num_connections=200
+num_io_threads=10
+num_connections=500
 
 # Define the array of ports
 for ((i=0; i<$n_ports; i++)); do
@@ -21,7 +21,7 @@ start=$(date +%s.%N)
 # Loop through the array and run the command with each port
 # for port in "${PORTS[@]}"
 # do
-./pull "tcp://$ZMQ_CONNECT_IP:$port_base" $msg_size $n_msgs $num_io_threads $num_connections &
+./pull "tcp://$ZMQ_CONNECT_IP:$port_base" $msg_size $n_msgs $num_io_threads $num_connections
   # PIDS+=($!) # add the PID of the most recently backgrounded process to the PIDS array
 # done
 
--- scripts/pull_mt.sh
+++ scripts/pull_mt.sh
@@ -3,12 +3,14 @@
 cd /src/build
 
 n_msgs=$1
-n_msgs=1000000
-msg_size=200000
-n_pushing_threads=20
+n_msgs=100000
+msg_size=2000000
+n_pushing_threads=4
 n_ports=$n_pushing_threads
 n_msgs_per_port=$(($n_msgs / $n_pushing_threads))
-port_base=5535
+port_base=5525
+n_io_threads=10
+n_connections=1000
 
 # Define the array of ports
 for ((i=0; i<$n_ports; i++)); do
@@ -17,11 +19,12 @@ done
 
 # Start the timer
 start=$(date +%s.%N)
+echo $
 
 # Loop through the array and run the command with each port
 for port in "${PORTS[@]}"
 do
-  ./pull "tcp://$ZMQ_CONNECT_IP:$port" $msg_size $n_msgs &
+  ./pull "tcp://$ZMQ_CONNECT_IP:$port" $msg_size $n_msgs_per_port $n_io_threads $n_connections &
   PIDS+=($!) # add the PID of the most recently backgrounded process to the PIDS array
 done
 
@@ -34,5 +37,10 @@ done
 # End the timer and calculate data rate
 end=$(date +%s.%N)
 total_time=$(echo "$end - $start" | bc)
-data_sent=$(echo "($n_msgs_per_port * $n_ports * $msg_size) / $total_time / 1024 / 1024 / 1024 * 8" | bc)
-echo "Data sent: $data_sent Gbits/sec"
+throughput=$(echo "($n_msgs / $total_time)" | bc)
+data_sent=$(echo "($throughput * $msg_size * 8) / 1000000 " | bc)
+echo "---------------" 
+echo "Throughput: $throughput msgs/s"
+echo "Time: $total_time s" 
+echo "Data rate: $data_sent Mb/s"
+echo "---------------"
