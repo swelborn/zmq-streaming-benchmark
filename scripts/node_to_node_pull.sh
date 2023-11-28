20ce800980721010809d11919ce75fc058c346f6
Sam Welborn <swelborn@lbl.gov>
2023-11-28 06:41:43 -0800

    add node-to-node scripts

--- scripts/node_to_node_pull.sh
+++ scripts/node_to_node_pull.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-srun -n 1 -N 1 podman-hpc run --rm --net=host --env-file="/global/homes/s/swelborn/gits/zmq-streaming-benchmark/config/conf" -v /global/homes/s/swelborn/gits/zmq-streaming-benchmark/scripts:/scripts samwelborn/zmq-streaming-benchmark:716ccca /scripts/pull_mt.sh 100000
+srun -n 1 -N 1 podman-hpc run --rm --net=host --env-file="/global/homes/s/swelborn/gits/zmq-streaming-benchmark/config/conf" -v /global/homes/s/swelborn/gits/zmq-streaming-benchmark/scripts:/scripts samwelborn/zmq-streaming-benchmark:f32679f /scripts/pull_mt.sh 100000
--- scripts/node_to_node_push.sh
+++ scripts/node_to_node_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-srun -n 1 -N 1 podman-hpc run --rm --net=host -v /global/homes/s/swelborn/gits/zmq-streaming-benchmark/scripts:/scripts samwelborn/zmq-streaming-benchmark:716ccca /scripts/push_mt.sh
+srun -n 1 -N 1 podman-hpc run --rm --net=host -v /global/homes/s/swelborn/gits/zmq-streaming-benchmark/scripts:/scripts samwelborn/zmq-streaming-benchmark:f32679f /scripts/push_mt.sh
